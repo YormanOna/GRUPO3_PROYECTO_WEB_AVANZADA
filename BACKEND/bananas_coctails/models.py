8ca0e4d01f3554c04fd08ba3b079d4b7479dc6b7
Yorman <yjona@espe.edu.ec>
2024-07-17 02:01:43 -0500

    Silvimimida

--- BACKEND/bananas_coctails/models.py
+++ BACKEND/bananas_coctails/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Coctail(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+    cantidad = models.IntegerField()
+    garnishes = models.CharField(max_length=100)
+    mixers = models.CharField(max_length=100)
+class Paquetes(models.Model):
+    nombrePaquete = models.CharField(max_length=150)
+    numeroCocteles = models.IntegerField()
+    precio = models.IntegerField()
+    insumos = models.CharField(max_length=200)
+    numeroPersonas = models.IntegerField()
+    descripcion = models.CharField(max_length=200)
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    cedula = models.IntegerField()
+    domicilio = models.CharField(max_length=200)
+    telefono = models.IntegerField()
+    email = models.EmailField()
+class CuentaCliente(models.Model):
+    nombreUsuario = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    
